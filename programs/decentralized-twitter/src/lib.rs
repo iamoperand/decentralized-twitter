866a826468bd4b7388fe67f3341d9d720e63cc72
nikhil arora <narora200@gmail.com>
2022-05-26 23:15:01 +0530

    chore: update program_id

--- programs/decentralized-twitter/src/lib.rs
+++ programs/decentralized-twitter/src/lib.rs
@@ -1,12 +1,12 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Akgh7h4d9LzAgZVb4TxM4j86k3KDN5SuKWRDyjgJiozr");
+declare_id!("4u1pQBd6oZHYGHqHeyb9b3hW1XGeiz2msD7qiTcedNuP");
 
 #[program]
 pub mod twitter_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
